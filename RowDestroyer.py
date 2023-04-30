792a72ddd2d0ef48df0d572dc22d9cf7849c6932
Matteo Martelli <matteo.martelli13@studio.unibo.it>
2023-05-01 00:56:42 +0200

    modifiche al dataset

--- RowDestroyer.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('share-elec-produc-by-source.txt')
-df = df[df["Year"].isin([2017, 2018, 2019])]
-print(df)
-df.to_csv('share-elec-produc-by-source-FILTERED.txt', index=False)
