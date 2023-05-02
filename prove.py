3e368ab911f40a49925d764dc7ccb0b107cd3c39
Matteo Martelli <matteo.martelli13@studio.unibo.it>
2023-05-02 18:19:27 +0200

    Some update to the dataset

--- prove.py
+++ prove.py
@@ -1,22 +1,23 @@
 import networkx as nx
 import pandas as pd
 import csv
-Nations=[]
-TotalProduction=[]
+Nations2020=[]
+TotalProduction2020=[]
 with open('Electricity_Production_TWh2020_FINAL.txt', 'r') as file:
     TotalEnergy = csv.reader(file)
     next(TotalEnergy)
     for row in TotalEnergy:
         last_column = row[-1]  # seleziona l'ultima colonna
-        TotalProduction.append(last_column)
+        TotalProduction2020.append(last_column)
 
 with open("Imp-Exp_2020.txt", "r") as file:
     Names= csv.reader(file)
     next(file) # salta la prima riga
     
     row = next(file) # stampa la seconda riga
-    Nations.append(row)
-    Nations=[x.split(",") for x in Nations]
-print(Nations)
-print(TotalProduction)
+    Nations2020.append(row)
+    Nations2020=[x.split(",") for x in Nations2020]
+
+print(Nations2020)
+print(TotalProduction2020)
 
