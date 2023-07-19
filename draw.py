5a5c79a4059bcf905f52ecac47a2b231cadc30d0
Matteo Martelli <matteo.martelli13@studio.unibo.it>
2023-07-19 15:49:09 +0200

    creation of the file "draw.py" for the plot

---
+++ draw.py
@@ -0,0 +1,15 @@
+import networkx as nx
+
+option=int(input("Digit"))
+
+
+if option == 1:
+    import evolution
+elif option == 2:
+    import grid_2017_19
+elif option == 3:
+    import grid_2020
+elif option == 4:
+    import unweighted_2017_19
+
+
